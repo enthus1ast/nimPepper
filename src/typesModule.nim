94b9745a4c47638a47a7c8c778225510266e28af
David Krause <david@code0.xyz>
2019-10-24 13:07:01 +0200

    new loader first working call :)
    Signed-off-by: David Krause <david@code0.xyz>

--- src/typesModule.nim
+++ src/typesModule.nim
@@ -1,13 +1,6 @@
 # import pepperslaveImports
 import json, asyncdispatch
 export json, asyncdispatch
-
-
-type
-  ## The procedure modules must implement
-  SlaveCommandFunc*[T] = proc(obj: T, params: string = ""): Future[JsonNode]
-
-####################
 import typesPepperSlave
 import tables
 type
