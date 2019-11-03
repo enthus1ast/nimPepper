c044df886cc20f9e135a20e13b8f4b8df1d0b8e7
David Krause <david@code0.xyz>
2019-11-03 22:52:42 +0100

    wip on mwww.nim
    Signed-off-by: David Krause <david@code0.xyz>

---
+++ src/nimPepper/modules/defaults/mwww.nim
@@ -0,0 +1,52 @@
+# import ../../typesModuleSlave
+import ../../lib/pepperdImports
+import ../../lib/typesPepperd
+import ../../lib/moduleLoader
+import ../../lib/messages
+import ../../lib/netfuncs
+import ../../lib/pepperdFuncs
+import asynchttpserver # for the httpCallback and httpAdminCallback
+import strutils
+import times
+# import matcher
+
+# var module* {.exportc.} = newSlaveModule("defaults")
+var modmwww* {.exportc.} = newMasterModule("www")
+
+
+modmwww.initProc = proc(obj: Pepperd, params: string): Future[JsonNode] {.async, closure.} =
+  # asyncCheck obj.pingClients
+  echo "www INIT CALLED"
+
+# modmwww.boundCommands["dummy"] = proc(obj: Pepperd, params: string): Future[JsonNode] {.async, closure.} =
+#   echo "www COMMAND CALLED"
+#   return %* {"outp": "pong"}
+
+modmwww.slaveConnects = proc(obj: Pepperd, client: Client): Future[void] {.async, closure.} =
+  echo "WWW CLIENT CONNECTS:", client.peerAddr #, repr client
+  return
+
+modmwww.slaveDisconnects = proc(obj: Pepperd, client: Client): Future[void] {.async, closure.} =
+  echo "WWW CLIENT DISCONNECTS:", client.peerAddr #repr client
+  return
+
+modmwww.httpCallback = proc(obj: Pepperd, request: Request): Future[bool] {.async, closure.} =
+  ## returns true if the http request was handled by this callback  
+  if not ($request.url.path).startsWith("/www"):
+    return false
+  case $request.url.path
+  of "/www/slaves/linux/latest":
+    await request.respond(Http200, "latest linux")
+  of "/www/slaves/windows/latest":
+    await request.respond(Http200, "latest windows")
+  else:
+    return false
+  return true
+
+modmwww.httpAdminCallback = proc(obj: Pepperd, request: Request): Future[bool] {.async, closure.} =
+  ## returns true if the http request was handled by this callback
+  if ($request.url.path).startsWith("/www"):
+    await request.respond(Http200, "www")
+    return true
+  else:
+    return false
--- src/nimPepper/modules/masterModules.nim
+++ src/nimPepper/modules/masterModules.nim
@@ -5,9 +5,11 @@ import ../lib/typesPepperd
 # Modules
 import defaults/mping
 import dummy/mdummy
+import defaults/mwww
 
 proc register*(modLoader: ModLoader) =
   ## this function gets called by the pepperd, 
   ## every module that is registered here will be available in the final executable
   registerModule[MasterModule](modLoader, modmping)
   registerModule[MasterModule](modLoader, modmdummy)
+  registerModule[MasterModule](modLoader, modmwww)
